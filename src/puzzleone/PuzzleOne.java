932c8ef82228c0c2be30e413c184330afe85cc4b
Kaldoron <94761381+Kaldoron@users.noreply.github.com>
2025-01-23 14:46:50 +0100

    Riddle One Part 1 and 2 finished

--- src/puzzleone/PuzzleOne.java
+++ src/puzzleone/PuzzleOne.java
@@ -3,49 +3,89 @@ package puzzleone;
 public class PuzzleOne {
 
 	public static void main(String[] args) {
-		int[] inputonesorted = InputOne.inputone;
-		int[] inputtwosorted = InputTwo.inputtwo;
-		
-		boolean onesorted =true;
-		boolean twosorted =true;
-		
+
+		countappearance();
+
+	}
+
+	public static void countappearance() {
+		int appearsRight = 0;
+		int countedLeft = 0;
+		int similarityScore = 0;
+		while (countedLeft < 999) {
+			int countednumber = InputOne.inputone[countedLeft]; 
+			for (int i = 0; i < InputOne.inputone.length; i++) {
+				if (InputOne.inputone[countedLeft] == InputTwo.inputtwo[i]) {
+					System.out.println("Vorkommen von " + InputOne.inputone[countedLeft] + " = " + appearsRight);
+					appearsRight++;
+				}
+			}
+			similarityScore = similarityScore + (countednumber* appearsRight);
+			System.out.println("Similarity-Score = " + similarityScore);
+			countedLeft++;
+			appearsRight =0;
+		}
+
+	}
+
+	public static void printing(int toprint[]) {
+		for (int p = 0; p < toprint.length; p++) {
+			System.out.printf(toprint[p] + ",");
+		}
+		System.out.println("");
+		System.out.println(
+				"-----------------------------------------------------------------------------------------------");
+	}
+
+	public static void bubbleandsubtract() {
+		boolean onesorted = true;
+		boolean twosorted = true;
+
+		printing(InputOne.inputone);
+
+		printing(InputTwo.inputtwo);
+
 		do {
 			onesorted = false;
-			for(int i =1;i<inputonesorted.length;i++) {
-				if (inputonesorted[i]<inputonesorted[(i-1)]) {
-					int tempsave = inputonesorted[i];
-					inputonesorted[i] = inputonesorted[(i-1)];
-					inputonesorted[(i-1)] = tempsave;
+			for (int i = 1; i < InputOne.inputone.length; i++) {
+				if (InputOne.inputone[i] < InputOne.inputone[(i - 1)]) {
+					int tempsave = InputOne.inputone[i];
+					InputOne.inputone[i] = InputOne.inputone[(i - 1)];
+					InputOne.inputone[(i - 1)] = tempsave;
 					onesorted = true;
 				}
-				
+
 			}
-		} while(onesorted);
-		
+
+		} while (onesorted);
+
 		do {
 			twosorted = false;
-			for(int j =1;j<inputtwosorted.length;j++) {
-				if (inputtwosorted[j]<inputtwosorted[(j-1)]) {
-					int tempsave = inputtwosorted[j];
-					inputtwosorted[j] = inputtwosorted[(j-1)];
-					inputtwosorted[(j-1)] = tempsave;
-					onesorted = true;
+			for (int j = 1; j < InputTwo.inputtwo.length; j++) {
+				if (InputTwo.inputtwo[j] < InputTwo.inputtwo[(j - 1)]) {
+					int tempsave = InputTwo.inputtwo[j];
+					InputTwo.inputtwo[j] = InputTwo.inputtwo[(j - 1)];
+					InputTwo.inputtwo[(j - 1)] = tempsave;
+					twosorted = true;
 				}
-				
+
 			}
-		} while(twosorted);
-		
-		int totaldistance =0;
-		
-		for (int k = 0; k < inputonesorted.length;k++ ) {
-			if (inputonesorted[k]<inputtwosorted[k]) {
-				totaldistance += (inputtwosorted[k]-inputonesorted[k]);
-			}
-			if (inputonesorted[k]>inputtwosorted[k]) {
-				totaldistance += (inputonesorted[k]-inputtwosorted[k]);
+		} while (twosorted);
+
+		printing(InputOne.inputone);
+
+		printing(InputTwo.inputtwo);
+
+		int totaldistance = 0;
+
+		for (int k = 0; k < InputOne.inputone.length; k++) {
+			if (InputOne.inputone[k] < InputTwo.inputtwo[k]) {
+				totaldistance = totaldistance + (InputTwo.inputtwo[k] - InputOne.inputone[k]);
+			} else if (InputOne.inputone[k] > InputTwo.inputtwo[k]) {
+				totaldistance = totaldistance + (InputOne.inputone[k] - InputTwo.inputtwo[k]);
 			}
 		}
-		System.out.println("Gesamtdistanz = "+totaldistance);
+		System.out.println("Gesamtdistanz = " + totaldistance);
 
 	}
 
